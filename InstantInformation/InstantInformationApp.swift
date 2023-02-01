3797dacab81b77119e669353c698c5f85b6363b7
Bruce Rick <bruce.rick@thescore.com>
2023-02-01 18:34:45 -0500

    Added Xcode project
    - Added SwiftLint
    - Added ComposableArchitecture

---
+++ InstantInformation/InstantInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantInformationApp.swift
+//  InstantInformation
+//
+//  Created by Bruce Rick on 2022-12-31.
+//
+
+import SwiftUI
+
+@main
+struct InstantInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
